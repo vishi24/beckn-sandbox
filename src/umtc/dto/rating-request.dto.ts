a1482ea9825f69c85cf93cd47a3c2e8b12c777fe
prasad-takale-eminds <84436945+prasad-takale-eminds@users.noreply.github.com>
2023-05-25 00:19:02 +0530

    Revert "updated response for umtc"

--- src/umtc/dto/rating-request.dto.ts
+++ src/umtc/dto/rating-request.dto.ts
@@ -5,5 +5,15 @@ export class RatingRequestDto extends MobilityRequestDto {
         rating_category: string;
         id: string;
         value: string;
+        feedback_form: [
+            {
+                id: string;
+                parent_id: string;
+                question: string;
+                answer: string;
+                answer_type: string;
+            },
+        ];
+        feedback_id: string;
     };
 }
