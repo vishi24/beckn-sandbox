01f00b098ca87df1b1e1447bd035992b795d4895
shreyvishal <shreya.vishal@eminds.ai>
2023-10-27 19:53:22 +0530

    Added:Initial Commit for Industry 4.0

---
+++ src/industry-4.0/3D-printing/3D-printing.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@nestjs/common";
+import { Industry4_0ApiInterface } from "../industry-4.0-api.interface";
+import { Industry_4_0_Request_DTO } from "../dto/request.dto";
+
+@Injectable()
+export class Three_D_PritingService implements Industry4_0ApiInterface {
+    search = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
+        return {};
+    };
+    select = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
+        return {};
+    };
+    init = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
+        return {};
+    };
+    confirm = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
+        return {};
+    };
+    status = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
+        return {};
+    };
+    cancel = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
+        return {};
+    };
+    update = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
+        return {};
+    };
+    support = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
+        return {};
+    };
+    rating = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
+        return {};
+    };
+    track = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
+        return {};
+    };
+}
---
+++ src/industry-4.0/industry-4.0.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from "@nestjs/common";
+
+@Controller("industry-4.0")
+export class Industry40Controller {}
