5d7ed6f5ee99fa3ef6b32dd2bcfef1afded1e54a
rajaneeshk90 <rajaneeshk90@gmail.com>
2023-10-28 19:54:05 +0530

    adding the request and response files

--- src/industry-4.0/3D-printing/3D-printing.service.ts
+++ src/industry-4.0/3D-printing/3D-printing.service.ts
@@ -4,36 +4,46 @@ import { Industry_4_0_Request_DTO } from "../dto/request.dto";
 import * as SearchResponse from "./response/response.search.json";
 import * as InitResponse from "./response/response.init.json";
 
+import * as SearchResponse from "./response/response.search.json";
+import * as InitResponse from "./response/response.init.json";
+import * as ConfirmResponse from "./response/response.confirm.json";
+//select
+import * as StatusResponse from "./response/response.status-dispatched.json";
+import * as UpdateResponse from "./response/response.update.json";
+import * as SupportResponse from "./response/response.support.json";
+import * as CancelResponse from "./response/response.cancel.json";
+import * as TrackResponse from "./response/response.track.json";
+
 @Injectable()
-export class Three_D_PritingService implements Industry4_0ApiInterface {
+export class Three_D_PrintingService implements Industry4_0ApiInterface {
     search = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
-        return SearchResponse;
+        return {SearchResponse};
     };
     select = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
         return {};
     };
     init = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
-        return InitResponse;
+        return {InitResponse};
     };
     confirm = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
-        return {};
+        return {ConfirmResponse};
     };
     status = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
-        return {};
+        return {StatusResponse};
     };
     cancel = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
-        return {};
+        return {CancelResponse};
     };
     update = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
-        return {};
+        return {UpdateResponse};
     };
     support = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
-        return {};
+        return {SupportResponse};
     };
     rating = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
         return {};
     };
     track = (industry_4_0_Request_DTO: Industry_4_0_Request_DTO) => {
-        return {};
+        return {TrackResponse};
     };
 }
