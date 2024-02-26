fff75c7feffe65bd599b19543ec556aee3aa3441
shreyvishal <shreya.vishal@eminds.ai>
2024-02-26 12:44:04 +0530

    Added: Integration of Machine Simulator for Industry 4.0

--- src/industry-4.0/constants.ts
+++ src/industry-4.0/constants.ts
@@ -1,1 +1,1 @@
-export const INDUSTRY_4_MACHINE_SIMULATOR_URL = "http://localhost:3030/machine";
+export const INDUSTRY_4_MACHINE_SIMULATOR_URL = "https://industry-4-machine-simulator-dev.becknprotocol.io/machine";
